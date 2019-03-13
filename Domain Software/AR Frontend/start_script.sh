20c0f3d710a845eceed5402b7bec9be14b553a33
Andrei Miclaus <frostyandy2k@yahoo.com>
2019-03-13 12:17:35 +0100

    first commit

---
+++ Domain Software/AR Frontend/start_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+openssl rsa -passin pass:x -in server.pass.key -out server.key
+
+openssl req -new -key server.key -out server.csr \
+    -subj "/C=DE/ST=BW/L=Karlsruhe/O=ScaleIT/OU=IT D/CN=example.com"
+openssl x509 -req -days 365 -in server.csr -signkey server.key -keyout key.pem -out cert.pem # server.crt
+
+npm start
+
+exit 0
